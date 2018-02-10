14dc3c2704ce4883260121eddc44ddf60d8eeb16
Mark Alaniz <markalaniz000@gmail.com>
2018-02-10 00:37:10 -0600

    working on about me landing page

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,14 @@
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-function openNav() {
-  $("#mySidenav").css("width", "250px");
-  $("#main").css("marginLeft", "250px");
-}
-
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-function closeNav() {
-  $("#mySidenav").css("width", "0px");
-  $("#main").css("marginLeft", "0px");
-}
+$(document).ready(function() {
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+  $("#openNav").on("click", function() {
+    $("#mySidenav").css("width", "250px");
+    // $("#main").css("marginLeft", "250px");
+    $("#openNav").hide();
+  });
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+  $("#closeNav").on("click", function() {
+    $("#mySidenav").css("width", "0px");
+    // $("#main").css("marginLeft", "0px");
+    $("#openNav").show();
+  });
+});
