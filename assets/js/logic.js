cabf061fbee4b7f065ac3932885188a9603b34be
Mark Alaniz <markalaniz000@gmail.com>
2018-02-19 01:57:20 -0600

    trying to toggle menu overlay in navbar and resize function won't until changed on load

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,29 @@
 $(document).ready(function() {
-
   // change places of brand name and collapsable navbar at screen width
-  $(window).resize(function(){
+  $(window).resize(function() {
     if ($(window).width() <= 767) {
-      $(".navbar-brand").detach().prependTo("#nav-container");
+      $(".navbar-brand")
+        .detach()
+        .prependTo("#nav-container");
     } else {
-      $(".navbar-brand").detach().appendTo("#nav-container");
+      $(".navbar-brand")
+        .detach()
+        .appendTo("#nav-container");
     }
   });
+
+  // collapsed nav menu for mobile and smaller screens
+  $("#toggle-drop").on("click", function() {
+    if ($("#toggle-drop").data('overlay')) {
+      $("#myNav").css("height", "0%");
+      $('#toggle-drop').data('overlay', "false");
+    } else {
+      $("#myNav").css("height", "100%");
+      $('#toggle-drop').data('overlay', "true");
+    }
+  });
+
+  // $("#toggle-drop").on("click", function() {
+  //   $("#myNav").css('height', '0%');
+  // });
 });
